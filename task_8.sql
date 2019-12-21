7c3b2fd3825e41706533c0b0ef88e83123591e65
Andrey Bukhtoyarov <andreymedoed@gmail.com>
2019-12-21 12:19:52 +0300

    8 задание сделано

--- task_8.sql
+++ task_8.sql
@@ -27,7 +27,7 @@ BEGIN
       VALUES ((SELECT resume_id FROM resume WHERE resume.resume_body_id = old.resume_body_id),
               id,
               (SELECT user_id FROM resume WHERE resume.resume_body_id = old.resume_body_id),
-              (SELECT creation_time FROM resume WHERE resume.resume_body_id = old.resume_body_id),
+              now(),
               false
              );
       UPDATE resume SET last_change_time = now()
@@ -44,17 +44,30 @@ $res_body$ LANGUAGE plpgsql;
 CREATE TRIGGER update_resume BEFORE UPDATE OR DELETE ON resume_body FOR EACH ROW
 EXECUTE PROCEDURE execute();
 
+-- Обновляем имя резюме по заданному resume_id
 UPDATE resume_body SET name = ('New Name 1' || quote_literal(random() * 10))
 WHERE resume_body_id = (SELECT resume_body_id FROM resume WHERE resume.resume_id = 5 AND resume.active = true);
 
-SELECT resume_id,
-       last_change_time,
-       res_body.name AS new_title,
-        (CASE
-          WHEN resume.active = false
-          THEN res_body.name
-          END
-          ) AS old_title
-        FROM resume
-      JOIN resume_body AS res_body ON resume.resume_id = 5 AND resume.resume_body_id = res_body.resume_body_id
-   ORDER BY last_change_time;
+-- Выводим историю именений названий резюме по заданному resume_id
+SELECT resume_id, last_change_time, new_title, old_title FROM
+  (SELECT ROW_NUMBER() OVER (ORDER BY last_change_time DESC) AS rn,
+          resume_id,
+          last_change_time,
+          res_body.name AS new_title
+   FROM resume
+          JOIN resume_body AS res_body ON resume.resume_id = 5 AND resume.resume_body_id = res_body.resume_body_id
+   ORDER BY last_change_time DESC
+  ) AS first
+    LEFT OUTER JOIN
+  (SELECT ROW_NUMBER() OVER (ORDER BY last_change_time DESC) AS rn,
+          res_body.name AS old_title
+   FROM resume_body AS res_body
+          JOIN resume AS res ON
+         resume_id = 5
+       AND res.resume_body_id = res_body.resume_body_id
+       AND res_body.resume_body_id <>
+           (SELECT resume_body_id FROM resume
+            WHERE resume.resume_id = 5 ORDER BY last_change_time DESC LIMIT 1
+           ) ORDER BY last_change_time DESC
+  ) AS second
+  USING (rn);
