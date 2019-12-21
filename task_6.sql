fd97c3719ee608a260e310baaff3fd41648d1b57
Andrey Bukhtoyarov <andreymedoed@gmail.com>
2019-12-21 21:56:37 +0300

    исправлен 6 таск

--- task_6.sql
+++ task_6.sql
@@ -1,16 +1,20 @@
-SELECT res_id, array_agg(arr), spec FROM
-  (SELECT res_id, arr, max(count), spec FROM
-    (SELECT res_id, arr, count(v_b_s.specialization_id) AS count, v_b_s.specialization_id AS spec FROM
-      (SELECT res_id, arr, vac_id, vac.vacancy_body_id AS v_b_id FROM
-        (SELECT res_id, arr, resp.vacancy_id AS vac_id FROM
-          (SELECT resume_id AS res_id, rbs.specialization_id AS arr FROM resume
-              JOIN resume_body_specialization AS rbs ON resume.active = true AND resume.resume_body_id = rbs.resume_body_id
-           ) AS first
-            JOIN response AS resp ON first.res_id = resp.resume_id
-         GROUP BY res_id, arr, vac_id) AS second
-          JOIN vacancy AS vac ON vac_id = vac.vacancy_id
-       GROUP BY res_id, arr, vac_id, v_b_id) AS third
-        JOIN vacancy_body_specialization AS v_b_s ON v_b_s.vacancy_body_id = v_b_id
-     GROUP BY res_id, arr, spec) AS fourth
-   GROUP BY res_id, arr, spec) AS fifth
-GROUP BY res_id, spec;
+WITH res_id_res_spec_arr AS (
+       SELECT resume_id AS res_id, array_agg(rbs.specialization_id) AS res_spec_arr FROM resume
+       JOIN resume_body_specialization AS rbs ON resume.active = true AND resume.resume_body_id = rbs.resume_body_id
+  GROUP BY res_id
+     ), plus_vac_id AS (
+  SELECT res_id, res_spec_arr, resp.vacancy_id AS vac_id FROM res_id_res_spec_arr
+       JOIN response AS resp ON res_id_res_spec_arr.res_id = resp.resume_id
+), plus_vac_body_id AS (
+  SELECT res_id, res_spec_arr, vac_id, vac.vacancy_body_id AS vac_body_id FROM plus_vac_id
+  JOIN vacancy AS vac ON vac_id = vac.vacancy_id
+     GROUP BY res_id, res_spec_arr, vac_id, vac_body_id
+), count_vac_body_spec AS (
+  SELECT res_id, res_spec_arr, count(vac_body_spec.specialization_id) AS count, vac_body_spec.specialization_id AS spec FROM plus_vac_body_id
+  JOIN vacancy_body_specialization AS vac_body_spec ON vac_body_spec.vacancy_body_id = vac_body_id
+   GROUP BY res_id, res_spec_arr, spec
+), max_count AS (
+  SELECT res_id, res_spec_arr, max(count) AS max, spec FROM count_vac_body_spec
+  GROUP BY res_id, res_spec_arr, spec
+)
+SELECT res_id, res_spec_arr, max, spec FROM max_count GROUP BY res_id, res_spec_arr, max, spec ORDER BY res_id;
